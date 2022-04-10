15a5695f160c565a4e292b901c4149222d783d5f
Tomáš Hübelbauer <tomas@hubelbauer.net>
2022-04-10 19:31:14 +0200

    Remove void async function wrapper
    Recent Node versions support TLA.

--- test.js
+++ test.js
@@ -1,7 +1,5 @@
 import winPid from './index.js';
 
-void async function () {
-  console.log('Start notepad.exe to see its PID here!');
-  const pid = await winPid('notepad.exe');
-  console.log('PID', pid);
-}()
+console.log('Start notepad.exe to see its PID here!');
+const pid = await winPid('notepad.exe');
+console.log('PID', pid);
